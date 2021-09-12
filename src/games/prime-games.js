39d45e47fa69a3dfbbba733aa07000068b52fa44
Aslan <korpeevaslan@gmail.com>
2021-09-12 05:28:53 +0500

    fix brain prime

--- src/games/prime-games.js
+++ src/games/prime-games.js
@@ -5,7 +5,7 @@ const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer
 
 const isPrime = (number) => {
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
-    if (number % 2 === 0) {
+    if (number % i === 0) {
       return false;
     }
   }
